f44827001572058c489238cac0e31516a73e8546
UnbanksyTV <45092543+unbanksytv@users.noreply.github.com>
2023-06-26 02:47:56 +0200

    Update index.tsx

--- pages/collection-rankings/index.tsx
+++ pages/collection-rankings/index.tsx
@@ -126,7 +126,7 @@ const IndexPage: NextPage<Props> = ({ ssr }) => {
             }}
           >
             <Text style="h4" as="h4">
-              Generative Art By Volume
+              Gen Art By Volume
             </Text>
             <Flex align="center" css={{ gap: '$4' }}>
               <CollectionsTimeDropdown
